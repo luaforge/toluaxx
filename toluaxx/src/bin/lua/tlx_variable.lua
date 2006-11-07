f5dc296aaf77ad1a997e7a92b76fdea1348a7c89
Phoenix RareSky <phoenix11@luaforge.net>
2006-11-07 16:36:12 +0000

    Bugfix export unsigned variables

--- toluaxx/src/bin/lua/tlx_variable.lua
+++ toluaxx/src/bin/lua/tlx_variable.lua
@@ -2,7 +2,7 @@
 -- Written by Waldemar Celes
 -- TeCGraf/PUC-Rio
 -- Jul 1998
--- $Id: tlx_variable.lua,v 1.2 2006-10-25 16:06:25 phoenix11 Exp $
+-- $Id: tlx_variable.lua,v 1.3 2006-11-07 16:36:12 phoenix11 Exp $
 
 -- This code is free software; you can redistribute it and/or modify it.
 -- The software provided hereunder is on an "as is" basis, and
@@ -249,7 +249,7 @@ function classVariable:supcode ()
 	 local t = isbasic(self.type)
 	 
 	 if not t and ptr=='' then val=val..'*' end
-	 val=val..'(('..self.mod..self.type
+	 val=val..'(('..self.mod.." "..self.type
 	 if not t then
 	    val=val..'*'
 	 end
